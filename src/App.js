9c67cf55428eefe8b60e3d1352dbe39ea0cf023e
Ju1ija <j.lipnickaja@gmail.com>
2021-11-24 14:36:42 +0000

    removed react logo from App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  return <Card />;
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+
+
+function Card() {
+  return (
+    <div>
+      <h1>HI</h1>
+      <p>Hello to you too</p>
+    </div>
+  )
+}
+
+export default Card;
