52422969eaaa0aa3ac6a61de71bff7bb5d9c0299
rosemarym2 <rosemarymay91@gmail.com>
2021-11-24 15:19:51 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import Card from "./components/Card.js"
+import List from "./List";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
-}
+const Basket = () => {
+    const [input, setInput] = useState("");
+    const [ item, setItem] = useState (["item 1", "item 2","item 3"]);
+    
+
+    const addItem = () =>{
+                let storedItem = [...item]
+                storedItem.push(input)
+                setItem(storedItem)
+            }
+    const removeItem = (index) => {
+                let storedItem = [...item]
+                storedItem.splice(index, 1)
+                setItem(storedItem)
+    }        
+
+    const changeHandler = (event) => {
+        setInput(event.target.value)
+    }
+    return (
+        <div className ="container">
+            <h1>My Notes:</h1>
+            <input type="text" placeholder="Add note here" onChange={changeHandler}/>
+            <button onClick={addItem}>add note</button>
+            <p>{input}</p>  
+            {item.map((item, index) => {
+            return (
+            <div key={index}>                   
+            <h2 className="note">{item}
+            <button className="delete-btn" onClick={() => removeItem (index)}><DeleteForeverIcon /></button></h2>
+            </div> 
+            )
+        })}
+        </div>
+    );
+
+
+export default App;
 //modal2 test
 
 export default App;
