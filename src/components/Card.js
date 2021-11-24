2df0211ab54d5f99a652053f6fe18712578a4303
Ju1ija <j.lipnickaja@gmail.com>
2021-11-24 15:13:34 +0000

    fetching URLs from the cat API

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,27 @@
-
+import { useEffect, useState } from "react";
 
 function Card() {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    handler();
+  }, []);
+
+  const handler = async () => {
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search");
+      const data = await response.json();
+      setImage(data[0].url);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div>
-      <h1>HI</h1>
-      <p>Hello to you too</p>
+      <img src={image} alt="" />
+      <h3>Name</h3>
+      <button>Price</button>
     </div>
   )
 }
