c88b1999028e5541ed23eb5dd00335882c301cbf
Ju1ija <j.lipnickaja@gmail.com>
2021-11-25 10:24:44 +0000

    removed comments from App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-
-import { useState } from "react";
 import './App.css';
 import Card from "./components/Card.js"
-import List from "./List";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-// const Basket = () => {
-//     const [input, setInput] = useState("");
-//     const [ item, setItem] = useState (["item 1", "item 2","item 3"]);
-    
-
-//     const addItem = () =>{
-//                 let storedItem = [...item]
-//                 storedItem.push(input)
-//                 setItem(storedItem)
-//             }
-//     const removeItem = (index) => {
-//                 let storedItem = [...item]
-//                 storedItem.splice(index, 1)
-//                 setItem(storedItem)
-//     }        
-
-
+function App() {
   const numOfCards = new Array(12).fill();
 
   return (
