7db84f8bbcac492b777e8f48b94d9262f6041d9c
Ju1ija <j.lipnickaja@gmail.com>
2021-11-26 08:22:40 +0000

    get cats in app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Card from "./components/Card.js"
+import { useEffect } from 'react';
+import { useState } from 'react';
+const faker = require('faker');
 
 function App() {
-  const numOfCards = new Array(12).fill();
+  const [data, setData] = useState([]);
+  const [basket, setBasket] = useState([]);
+
+  useEffect(() => {
+    getCats();
+  }, []);
+
+  const getCats = async () => {
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=20");
+      const data = await response.json();
+      setData(data.map((item =>
+      ({
+        url: item.url,
+        name: faker.name.firstName(),
+        price: faker.finance.amount()
+      }))));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const basketHandler = (item) => {
+    setBasket([...basket, item]);
+  }
 
   return (
     <div className="cats-on-sale">
-      {numOfCards.map((index) => {
+      {data.map((item) => {
         return (
-          <div key={index}>
-            <Card />
+          <div key={item.id}>
+            <div className="mainSet">
+              <img src={item.url} alt="cat on sale" />
+              <h2>{item.name}</h2>
+              <h3>£{item.price}</h3>
+              <p><a onClick={() => { }}
+              >click here to learn more</a></p>
+              <button onClick={() => basketHandler(item)}>BUY</button>
+            </div> :
+            <div className="infoSet">
+              <h2>{item.name}</h2>
+              <h3>£{item.price}</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia orci sapien, at posuere odio condimentum ac. Phasellus ut arcu et orci cursus mattis dignissim id dui.imentum sagittis.</p>
+              <button onClick={() => { }}>BACK</button>
+            </div>
           </div>
         )
       })}
--- src/components/Basket.js
+++ src/components/Basket.js
@@ -6,36 +6,37 @@ import ReactDOM from 'react-dom';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 
-const Basket = () =>{
-    
-    
-    
-    const [cookies, setCookie] = useCookies(['']);
-    const [open, setOpen] = useState(false);
+const Basket = () => {
+
+
+
+  const [cookies, setCookie] = useCookies(['']);
+  const [open, setOpen] = useState(false);
+
 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
-  
-//   const handle = (props) => {
-//     setCookie('Index', props.index, {path: '/'});
-//  };
+
+  //   const handle = (props) => {
+  //     setCookie('Index', props.index, {path: '/'});
+  //  };
 
   return (
     <div>
-      <button onClick={onOpenModal}><FontAwesomeIcon icon={faShoppingCart}/></button>
+      <button onClick={onOpenModal}><FontAwesomeIcon icon={faShoppingCart} /></button>
       <Modal open={open} onClose={onCloseModal} center>
         <h2>My Basket</h2>
         <p></p>
       </Modal>
     </div>
-    )
+  )
 }
 
 
 
 export default Basket
 
- 
+
 
 
 
