0d2d42ca166a3c8dd6fa96a498c963ded631e37e
Ju1ija <j.lipnickaja@gmail.com>
2021-11-26 09:58:14 +0000

    extract card

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ function App() {
   }
 
   const basketHandler = (item) => {
-    alert(item)
     setBasket([...basket, item]);
   }
 
@@ -50,42 +49,12 @@ function App() {
       <div className="cats-on-sale">
         {data.map((item, id) => {
           return (
-            <CatCard item={item} id={id} basketHandler={basketHandler} />
+            <Card item={item} id={id} basketHandler={basketHandler} />
           )
         })}
       </div>
     </div>
   )
-}
-
-const CatCard = (props) => {
-  const [info, setInfo] = useState(true);
-
-  return (<div className="container">
-    <div key={props.id}>
-      {info ? <MainSetItem item={props.item} open={() => { setInfo(false) }} buy={() => props.basketHandler(props.item)} />
-        : <InfoSetItem item={props.item} close={() => { setInfo(true) }} />}
-    </div>
-  </div>)
-}
-
-const MainSetItem = (props) => {
-  return (<div className="mainSet">
-    <img src={props.item.url} alt="cat on sale" />
-    <h2>{props.item.name}</h2>
-    <h3>£{props.item.price}</h3>
-    <p><a onClick={props.open}>click here to learn more</a></p>
-    <button onClick={props.buy}>BUY</button>
-  </div>)
-}
-
-const InfoSetItem = (props) => {
-  return (<div className="infoSet">
-    <h2>{props.item.name}</h2>
-    <h3>£{props.item.price}</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia orci sapien, at posuere odio condimentum ac. Phasellus ut arcu et orci cursus mattis dignissim id dui.imentum sagittis.</p>
-    <button onClick={props.close}>BACK</button>
-  </div>)
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,68 +2,35 @@ import './Card.css';
 import { useEffect, useState } from "react";
 const faker = require('faker');
 
-function Card() {
-  const [image, setImage] = useState("");
-  const [name, setName] = useState("");
+
+const Card = (props) => {
   const [info, setInfo] = useState(true);
-  const [price, setPrice] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [errorState, setErrorState] = useState({
-    error: false,
-    message: "",
-  });
 
-  useEffect(() => {
-    imageHandler();
-    nameHandler();
-    priceHandler();
-  }, []);
-
-  const imageHandler = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch("https://api.thecatapi.com/v1/images/search");
-      const data = await response.json();
-      setImage(data[0].url);
-      setLoading(false);
-    } catch (err) {
-      setErrorState({ error: true, message: err.message });
-    }
-  }
-
-  const nameHandler = () => {
-    const name = faker.name.firstName();
-    setName(name);
-  }
-
-  const priceHandler = () => {
-    const catPrice = faker.finance.amount();
-    setPrice(catPrice);
-  }
-
-  return (
-    <div className="container">
-      {info ?
-        <div className="mainSet">
-          {errorState.error ? <h3>{errorState.message}</h3>
-            : loading ? <div className="loader"></div>
-              : <img src={image} alt="cat on sale" />
-          }
-          <h2>{name}</h2>
-          <h3>£{price}</h3>
-          <p><a onClick={() => { setInfo(false) }}
-          >click here to learn more</a></p>
-          <button>BUY</button>
-        </div> :
-        <div className="infoSet">
-          <h2>{name}</h2>
-          <h3>£{price}</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia orci sapien, at posuere odio condimentum ac. Phasellus ut arcu et orci cursus mattis dignissim id dui.imentum sagittis.</p>
-          <button onClick={() => { setInfo(true) }}>BACK</button>
-        </div>
-      }
+  return (<div className="container">
+    <div key={props.id}>
+      {info ? <MainSetItem item={props.item} open={() => { setInfo(false) }} buy={() => props.basketHandler(props.item)} />
+        : <InfoSetItem item={props.item} close={() => { setInfo(true) }} />}
     </div>
-  )
+  </div>)
+}
+
+const MainSetItem = (props) => {
+  return (<div className="mainSet">
+    <img src={props.item.url} alt="cat on sale" />
+    <h2>{props.item.name}</h2>
+    <h3>£{props.item.price}</h3>
+    <p><a onClick={props.open}>click here to learn more</a></p>
+    <button onClick={props.buy}>BUY</button>
+  </div>)
+}
+
+const InfoSetItem = (props) => {
+  return (<div className="infoSet">
+    <h2>{props.item.name}</h2>
+    <h3>£{props.item.price}</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia orci sapien, at posuere odio condimentum ac. Phasellus ut arcu et orci cursus mattis dignissim id dui.imentum sagittis.</p>
+    <button onClick={props.close}>BACK</button>
+  </div>)
 }
 
 export default Card;
